8db2ad2cb1178f3962ee0a9a34da92d74b20b77d
Wei Xia <wei_xia@hotmail.com>
2017-10-30 21:18:51 -0400

    added some functions

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -6,28 +6,48 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood
 {
     public class Startup
     {
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(env.ContentRootPath).AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            //Greeters = new Greeter(Configuration);
+        }
+
+        public IConfiguration  Configuration { get; set; }
+
+        public IGreeter Greeters { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
+            services.AddSingleton(Configuration); //looks like we don't need to call this line at all, as other wise this should be added before IGreeter service is added
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseWelcomePage("/welcome");
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!!!");
+
+              //  throw new Exception("wrong here");
+                var message = greeter.GetGreeting();//Configuration["Greeting"];
+            await context.Response.WriteAsync(message);
             });
         }
     }
